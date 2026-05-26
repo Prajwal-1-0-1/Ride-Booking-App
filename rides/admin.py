1d5c4bf4a1dbb5b6b80edbbed59898fd4e56460f
Prajwal <175596070+prajwalys101@users.noreply.github.com>
2026-05-27 02:15:28 +0530

    creating tables

--- rides/admin.py
+++ rides/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Ride, Driver, Rider
 
-# Register your models here.
+admin.site.register(Ride)
+admin.site.register(Driver)
+admin.site.register(Rider)
+
+
